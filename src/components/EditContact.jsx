cd55cf3031bff9fa9995d097dbac91126a166e9c
Nakul Kamatkar <nak2002k@gmail.com>
2022-01-23 14:17:12 +0530

    final

--- src/components/EditContact.jsx
+++ src/components/EditContact.jsx
@@ -1,8 +1,57 @@
-import React from 'react';
-import { Link,useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link,useNavigate,useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const EditContact = () => {
+        const[name,setName] = useState("");
+        const[email,setEmail] = useState("");
+        const[number,setNumber] = useState("");
     const {id} = useParams();
+    const contacts = useSelector(state=>state
+    )
+    const dispatch = useDispatch();
+    const history = useNavigate();
+    const currentContact = contacts.find(
+        (contact) => contact.id === parseInt(id)
+      );
+      useEffect(()=>{
+          if (currentContact){
+              setName(currentContact.name)
+              setEmail(currentContact.email)
+              setNumber(currentContact.number)
+          }
+        
+
+      }, [currentContact])
+
+      const handleSubmit = (e)=>{
+        e.preventDefault();
+        const checkEmail=contacts.find(contact=> contact.id !== parseInt(id) && contact.email===email &&email);
+        const checkNumber=contacts.find(contact=> contact.id !== parseInt(id) &&  contact.number===parseInt(number) );
+        if (!email || !number || !name){
+            return toast.warning("Please fill in all details")
+        }
+        if (checkEmail){
+            return toast.error("This email already Exists!");
+    
+        }
+        if (checkNumber){
+            return toast.error("This number already Exists!");
+    
+        }
+        const data={
+            id: parseInt(id),
+            name,
+            email,
+            number,
+        }
+       dispatch({type:"UPDARE_CONTACT",payload:data});
+       toast.success("Contact updated added Successfully!")
+       history ("/")
+       
+    }
+     
   return <div className='container'>
   <h1 className='display-3 my-5  text-center'>
       Edit Contact {id}
@@ -11,15 +60,18 @@ const EditContact = () => {
 
   
   <div className='col-md-6 shadow mx-auto p-5'>
-      <form>
+      <form onSubmit={handleSubmit}>
           <div className='form-group'>
-              <input type="text" placeholder='Name' className='form-control'/>
+              <input type="text" placeholder='Name' className='form-control'
+               value={name} onChange={e=>setName(e.target.value)}/>
           </div>
           <div className='form-group'>
-              <input type="email" placeholder='Email'className='form-control'/>
+              <input type="email" placeholder='Email'className='form-control'
+               value={email} onChange={e=>setEmail(e.target.value)}/>
           </div>
           <div className='form-group'>
-              <input type="number" placeholder='Phone number'className='form-control'/>
+              <input type="number" placeholder='Phone number'className='form-control'
+               value={number} onChange={e=>setNumber(e.target.value)}/>
           </div>
           <div className='form-group'>
               <input type="submit" value='Update contact'className='btn  btn-dark m-2'/>
@@ -28,10 +80,10 @@ const EditContact = () => {
               <Link to ="/" className='btn btn-dark btn-danger  ml-3'> Cancel </Link>
           </div>
         </form>
-  </div>
+  </div>  
+</div>
 </div>
 
-</div>;
 };
 
 export default EditContact;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Home = () => {
     const contacts = useSelector(state=>state);
+    const dispatch = useDispatch();
+    const deleteContact = (id)=>{
+        dispatch({type:"DELETE_CONTACT",payload:id});
+        toast.success("Contact deleted successfully!");
+    }
   return <div className='container'>
       <div className='row'>
 
@@ -33,7 +39,7 @@ const Home = () => {
                                   <td>{contact.number}</td>
                                   <td>
                                   <Link to={{pathname: `/edit/${contact.id}` }} className='btn btn-small btn-primary'>Edit</Link>
-                                      <button type='button' className='btn btn-small btn-danger m-1'>Delete</button>
+                                      <button type='button' onClick={()=>deleteContact(contact.id)} className='btn btn-small btn-danger m-1'>Delete</button>
                                   </td>
                               </tr>
                           ))
